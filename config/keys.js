f42a115e31fbf0896e90e86d2b67878983938621
naduhrin78 <naduhrin78@gmail.com>
2018-12-07 20:48:24 +0530

    First push, basic routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoDbKey: "mongodb://naduhrin:urmywitch09@ds127704.mlab.com:27704/merndb"
+};
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
+
+const router = express.Router();
+
+const app = express();
+
+// Port config
+const port = process.env.PORT || 3000;
+
+// DB key
+const dbKey = require("./config/keys").mongoDbKey;
+
+app.get("/", (req, res) => res.send("Hello world!"));
+
+// Use routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profiles", profiles);
+
+// Start the server
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// Connect to mongoDb
+mongoose
+  .connect(
+    dbKey,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Successfully connected to MongoDb"))
+  .catch(err => console.log(`Failed to connect to db. ${err}`));
